404da9ac5afd94c0ffee3cd7e28f71c4156846ca
olivialin21 <s110819023@stu.ntue.edu.tw>
2022-04-03 19:10:04 +0800

    add swiper

--- js/main.js
+++ js/main.js
@@ -4,4 +4,22 @@ $(document).ready(function(){
     $(this).parent().toggleClass('active');
     $(this).parent().find('ul').slideToggle();
   })
+
+  const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    loop: true,
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+    },
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+    autoplay: {
+      delay: 5000,
+    },
+    speed: 800,
+  });
 })
