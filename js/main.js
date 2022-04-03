be147d668ee9372e04ec177aa9d0da6ee444964c
olivialin21 <s110819023@stu.ntue.edu.tw>
2022-04-04 00:41:42 +0800

    add top btn

--- js/main.js
+++ js/main.js
@@ -4,14 +4,22 @@ $(document).ready(function(){
     event.preventDefault();
     $(this).parent().toggleClass('active');
     $(this).parent().find('ul').stop().slideToggle();
-  })
+  });
   // 點選項或空白處時，選單收起
   $(document).click(function(event){
     if(!$('.products').is(event.target) && $('.products').has(event.target).length === 0){
+      $('.products').parent().removeClass('active');
       $('.products').parent().find('ul').slideUp();  //滑動消失
     }
   });
+  // top
+  $('.top').click(function(){
+    $('html,body').animate({
+      scrollTop: 0
+    }, 300);
+  });
 
+  // swiper
   const swiper = new Swiper('.swiper', {
     // Optional parameters
     loop: true,
@@ -30,6 +38,7 @@ $(document).ready(function(){
     speed: 800,
   });
 
+  // lightBox
   lightbox.option({
     'resizeDuration': 200,
     'wrapAround': true,
